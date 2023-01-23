b2d649a5bd71d641240514406f568fdaeb2324a2
小松原達哉 <komatsubara@BurgerKing.local>
2023-01-24 02:03:59 +0900

    respo

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,50 +1,56 @@
 <template>
 <div id="app">
+    <v-app>
 
-<section class="layout">
-    <div class="header">
-        <p>KOMATSUBARA TATSUYA</p>
-    </div>
-    <div class="leftSide">
-        <nav>
-            <router-link to="/">Home</router-link>
-            <router-link to="/about">About</router-link>
-        </nav>
-    </div>
-    <div class="body">
+        <section class="lo">
+            <v-container>
+                <v-layout class="header">
+                    <v-flex xs12>
+                        <h1>KOMATSUBARA TATSUYA</h1>
+                    </v-flex>
+                </v-layout>
 
-    <router-view />
-    </div>
-    <div class="rightSide">4</div>
-    <div class="footer">5</div>
-</section>
+                <v-layout class="body">
+                    <v-flex>
+                        <router-view />
+                    </v-flex>
 
-    <footer>
-        <a>&copy; 2022 Komatsubara Tatsuya</a>
-    </footer>
+                </v-layout>
+
+            </v-container>
+        </section>
+        <footer>
+            <a>&copy; 2022 Komatsubara Tatsuya</a>
+        </footer>
+    </v-app>
 </div>
 </template>
 
+<!--script>
+export default {
+    name: 'App',
+
+    data: () => ({
+        //
+    })
+}
+</script-->
+
 <style>
-
-.layout {
-    width: 1366px;
-    height: 768px;
-
+.lo {
     display: grid;
     grid:
-    "header header header" auto
-    "leftSide body rightSide" 1fr
-    "footer footer footer" auto
-    / auto 1fr auto;
+        "header header header"auto "leftSide body rightSide"1fr "footer footer footer"auto / auto 1fr auto;
     gap: 8px;
 }
 
-.header { grid-area: header; }
-.leftSide { grid-area: leftSide; }
-.body { grid-area: body; }
-.rightSide { grid-area: rightSide; }
-.footer { grid-area: footer; }
+.container {
+    max-width: 100%;
+}
+
+.header {
+    grid-area: header;
+}
 
 #app {
     font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -54,8 +60,17 @@
     color: #2c3e50;
 }
 
-#app p {
+#app h1 {
     text-align: left;
+    color: #fff;
+    font-family: Inter, Impact, Arial, sans-serif;
+    font-weight: bold;
+    font-size: 20px;
+    -webkit-text-stroke: 0.5px black;
+    text-shadow: 2px 2px 3px #76FF03;
+}
+
+#app h2 {
     color: #fff;
     font-family: Inter, Impact, Arial, sans-serif;
     font-weight: bold;
